a1dd4f984fd41863667331dd2a4bbca8b8954596
Ian Burgan <burganian@gmail.com>
2025-12-17 17:50:36 -0500

    Solve day 10 part two

--- 10.py
+++ 10.py
@@ -1,46 +1,61 @@
 #!/usr/bin/env python3
 
-from collections import deque
+from collections import defaultdict
+from itertools import chain, combinations
+from math import inf
 
 import utils
 
 lines = utils.parse_input("10.in")
 
+
+def powerset(items: list[tuple[int, ...]]):
+    return chain.from_iterable(combinations(items, r) for r in range(len(items) + 1))
+
+
 part_one = 0
 part_two = 0
 
-for line in lines:
+for i, line in enumerate(lines):
     parts = line.split()
     diagram = parts[0]
     buttons = parts[1:-1]
     joltage = parts[-1]
 
-    diagram = diagram[1:-1]
-    diagram = set(i for i, x in enumerate(diagram) if x == "#")
+    diagram = frozenset(i for i, x in enumerate(diagram[1:-1]) if x == "#")
 
     buttons = [x[1:-1].split(",") for x in buttons]
     buttons = [tuple(map(int, x)) for x in buttons]
 
-    q = deque([[x] for x in buttons])
-    while q:
-        curr = q.popleft()
+    joltage = tuple(int(x) for x in joltage[1:-1].split(","))
 
-        on_lights = set()
-        for button in curr:
-            for light in button:
-                if light in on_lights:
-                    on_lights.discard(light)
-                else:
-                    on_lights.add(light)
+    solutions = defaultdict(list)
+    for button_group in powerset(buttons):
+        press_count = len(button_group)
+        deltas = [0] * len(joltage)
+        for button in button_group:
+            for i in button:
+                deltas[i] += 1
 
-        if on_lights == diagram:
-            part_one += len(curr)
-            break
+        odd_positions = frozenset(i for i, x in enumerate(deltas) if x % 2 != 0)
+        solutions[odd_positions].append((press_count, deltas))
+    part_one += min(x[0] for x in solutions[diagram])
 
-        for button in buttons:
-            if button not in curr:
-                q.append(curr + [button])
+    def solve(remaining: tuple[int, ...]) -> float:
+        if sum(remaining) == 0:
+            return 0
 
-print(part_one)
-# print("Part One:", part_one)
-# print("Part Two:", part_two)
+        result = inf
+        odd_positions = frozenset(i for i, x in enumerate(remaining) if x % 2 != 0)
+        for press_count, deltas in solutions[odd_positions]:
+            next = tuple((x - y) // 2 for x, y in zip(remaining, deltas))
+            if any(x < 0 for x in next):
+                continue
+            result = min(result, press_count + (2 * solve(next)))
+
+        return result
+
+    part_two += solve(joltage)
+
+print("Part One:", part_one)
+print("Part Two:", part_two)
